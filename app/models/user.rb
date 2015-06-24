b866845482678804b47363f581e93a1485c82bd5
Max Calabrese <max.calabrese@ymail.com>
2015-06-24 02:08:34 +0200

    add usertype

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates :email, :username, uniqueness: true
+  validates_presence_of :organization, unless: -> { usertype == 1 }
+  belongs_to :organization
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,26 @@ RSpec.describe User, type: :model do
   it { should validate_uniqueness_of :username }
   it { should respond_to :password }
   it { should respond_to :password_confirmation }
+  it { should belong_to :organization }
+
+  let(:valid_attributes) {{
+      email: 'test@example.com',
+      username: 'testy',
+      password: 'abcd1234',
+      password_confirmation: 'abcd1234'
+  }}
+
+  it 'validates the presence of org. if the user type is not 1' do
+    user = User.new(valid_attributes.merge(usertype: 2))
+    user.valid?
+    expect(user.errors.messages).to have_key :organization
+  end
+
+  it 'does not validate the presence of org. if the usertype is 1' do
+    user = User.new(valid_attributes.merge(usertype: 1))
+    user.valid?
+    expect(user.errors.messages).to_not have_key :organization
+  end
+
+
 end
