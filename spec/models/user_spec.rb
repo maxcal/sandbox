b77972521cfbb3acf65b024b56bd631d880c08d1
Max Calabrese <max.calabrese@ymail.com>
2015-06-24 04:16:48 +0200

    validate user organisation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe User, type: :model do
     user = User.new(valid_attributes.merge(usertype: 2))
     user.organization = Organization.create(name: 'M & M Enterprises')
     user.valid?
-    expect(user.errors.messages).to have_key :organization
+    expect(user.errors["organization"]).to_not include "can't be blank"
   end
 
   it 'does not validate the presence of org. if the usertype is 1' do
