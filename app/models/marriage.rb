bd948ebe85a22a0469bccdb24ef01ceac3ae5376
Max Calabrese <max.calabrese@ymail.com>
2015-07-24 19:44:15 +0200

    add length validation to marriage

--- app/models/marriage.rb
+++ app/models/marriage.rb
@@ -1,4 +1,4 @@
 class Marriage < ActiveRecord::Base
   has_and_belongs_to_many :users
-#  validates :users, length: { minimum: 2, maximum: 2 }
+  validates :users, length: { minimum: 2, maximum: 2 }
 end
