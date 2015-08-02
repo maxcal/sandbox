01e1b503c21a4b7db0f54d554e860fea82d5e018
Max Calabrese <max.calabrese@ymail.com>
2015-08-02 21:19:46 +0200

    state_code is a instance method - not class

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -5,20 +5,17 @@ RSpec.describe Foo, type: :model do
   let(:foo) { build(:foo) }
   it { should validate_presence_of :state }
 
-  describe '.state_code' do
+  describe '#state_code' do
     it 'returns the state code' do
       expect(foo.state_code).to eq 'AZ'
     end
   end
 
-  describe '.state_code=' do
-
+  describe '#state_code=' do
     let!(:vt) { State.create(state_code: 'VT') }
-
     it 'allows you to set the state with a string' do
       foo.state_code = 'VT'
       expect(foo.state).to eq vt
     end
   end
-
 end
